69d12c9c09f1b83ce90e735d347410e34db6f674
AykQ <asdesra@gmail.com>
2025-03-09 04:00:41 +0300

    calismalar eklendi.

--- App.tsx
+++ App.tsx
@@ -1,131 +1,94 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, { useState } from 'react';
+import { Image, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { Linking } from 'react-native';
+import { TextInput } from 'react-native';
+import CustomButton from './components/customButton'
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+const HelloWorldApp = () => {
+    return (
+        <SafeAreaView style={styles.container}>
+            <View style={styles.dataContainer}>
+                <Text style={styles.text}>merhaba</Text>
+                <Text style={styles.subtitle}>bir takım bilgiler</Text>
+                <Text>{isimler('burak', 'tuncel', 'fü')}</Text>
+                <TextInput style={styles.textinput} placeholder='test'>
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+                </TextInput>
+            </View>
+            <View>
+                <ClickTracker></ClickTracker>
+                <Print></Print>
+            </View>
+        </SafeAreaView>
+    );
+};
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+const ClickTracker = () => {
+    const [count, setCount] = useState(0)
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
+    return (
+        <View>
+            <Text style={styles.clicktracker}>butona { count } kez basildi</Text>
+            <CustomButton title='sayac icin tikla' onPress={ () => setCount(count+1) }></CustomButton>
+        </View>
+    )
 }
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+const Print = () => {
+    const [message, setMessage] = useState('')
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+    const PrintButtonPress = () => {
+        setMessage('test yazisi')
+    }
 
-  /*
-   * To keep the template simple and small we're adding padding to prevent view
-   * from rendering under the System UI.
-   * For bigger apps the reccomendation is to use `react-native-safe-area-context`:
-   * https://github.com/AppAndFlow/react-native-safe-area-context
-   *
-   * You can read more about it here:
-   * https://github.com/react-native-community/discussions-and-proposals/discussions/827
-   */
-  const safePadding = '5%';
-
-  return (
-    <View style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        style={backgroundStyle}>
-        <View style={{paddingRight: safePadding}}>
-          <Header/>
+    return(
+        <View>
+            <CustomButton title='ekrana yazdir' onPress={PrintButtonPress}></CustomButton>
+            {message ? <Text style={styles.printText}>{message}</Text> : null}
         </View>
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-            paddingHorizontal: safePadding,
-            paddingBottom: safePadding,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </View>
-  );
+    )
+}
+
+const isimler = (isim: string, soyisim: string, okul: string) => {
+    return isim + " " + soyisim + " " + okul;
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+    container: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1,
+    },
+    text: {
+        fontSize: 45,
+        fontWeight: 'bold',
+    },
+    subtitle: {
+        fontWeight: 'normal',
+        fontSize: 30,
+    },
+    textinput: {
+        width: 250,
+        height: 50,
+        borderWidth: 1,
+        borderRadius: 15,
+        paddingHorizontal: 15
+    },
+    dataContainer: {
+/*         alignItems: 'center',
+        justifyContent: 'center' */
+    },
+    clicktracker: {
+        marginTop: 10,
+        marginBottom: 10
+    },
+    printText: {
+        marginTop: 10,
+        fontSize: 18,
+        fontWeight: 'bold',
+      },
+})
 
-export default App;
+export default HelloWorldApp;
---
+++ components/customButton.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import { TouchableOpacity, Text, StyleSheet } from "react-native";
+
+const CustomButton: React.FC<ButtonProps> = ({ title, onPress }) => {
+    return(
+        <TouchableOpacity style = { styles.button } onPress={onPress}>
+            <Text style = { styles.buttonText }> {title} </Text>
+        </TouchableOpacity>
+    );
+};
+
+type ButtonProps = {
+    title: string,
+    onPress: () => void
+}
+
+const styles = StyleSheet.create({
+    button: {
+        backgroundColor: '#9acd32',
+        alignItems: 'center',
+        borderRadius: 8,
+        padding: 10,
+    },
+    buttonText: {
+        color: 'black',
+        fontSize: 16,
+        fontWeight: "bold"
+    }
+});
+
+export default CustomButton;
